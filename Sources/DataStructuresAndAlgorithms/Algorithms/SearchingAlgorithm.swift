cf9d27451a402949f027c19a82b5346feef2737b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-09-14 13:20:48 +0500

    Add binary search

---
+++ Sources/DataStructuresAndAlgorithms/Algorithms/SearchingAlgorithm.swift
@@ -0,0 +1,29 @@
+struct SearchingAlgorithm {
+    // In computer science, binary search, also known as half-interval search,[1] logarithmic search,[2] or binary
+    // chop,[3] is a search algorithm that finds the position of a target value within a sorted array.[4][5] Binary
+    // search compares the target value to the middle element of the array. If they are not equal, the half in which
+    // the target cannot lie is eliminated and the search continues on the remaining half, again taking the middle
+    // element to compare to the target value, and repeating this until the target value is found. If the search
+    // ends with the remaining half being empty, the target is not in the array.
+    // https://en.wikipedia.org/wiki/Binary_search_algorithm
+    //
+    // Worst complexity: O(logn)
+    // Average complexity: O(logn)
+    // Best complexity: O(1)
+    // Space complexity: O(1) (a.k.a memory usage)
+    func binarySearch<T: Comparable>(_ x: T, n: [T]) -> Int? {
+        var l = 0
+        var h = n.count - 1
+        while l <= h {
+            let m = l + (h - l) / 2
+            if n[m] == x {
+                return m
+            } else if n[m] < x {
+                l = m + 1
+            } else {
+                h = m - 1
+            }
+        }
+        return nil
+    }
+}
--- Sources/DataStructuresAndAlgorithms/main.swift
+++ Sources/DataStructuresAndAlgorithms/main.swift
@@ -2,6 +2,7 @@ import Foundation
 
 let c = 20
 var n: [Int] = (0..<c).map { _ in .random(in: 0...c) }
+let x = Int.random(in: 0...c)
 
 let sortingAlgorithm = SortingAlgorithm()
 print("Selection sort: \(sortingAlgorithm.selectionSort(n))")
@@ -9,3 +10,6 @@ print("Insertion sort: \(sortingAlgorithm.insertionSort(n))")
 print("Bubble sort: \(sortingAlgorithm.bubbleSort(n))")
 print("Merge sort: \(sortingAlgorithm.mergeSort(n))")
 print("Quick sort: \(sortingAlgorithm.quickSort(n))")
+
+let searchingAlgorithm = SearchingAlgorithm()
+print("Binary search: \(searchingAlgorithm.binarySearch(x, n: sortingAlgorithm.quickSort(n)))")
